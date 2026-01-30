070750ec1211e490ab0449971645f0dae343a219
ZaighumCheema47 <zaighum.cheema01@gmail.com>
2026-01-30 16:34:06 +0500

    Update closing.py

--- pages/closing.py
+++ pages/closing.py
@@ -8,7 +8,7 @@ import gspread
 
 from google.oauth2.service_account import Credentials
 
-from .printing_logic import trigger_thermal_print
+from printing_logic import trigger_thermal_print
 
 import re
 
